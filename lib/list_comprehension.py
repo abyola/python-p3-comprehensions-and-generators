9003b79e1599bfbaf15e90b6aab0cc0ce00e6ff2
abyola <abyola46@gmail.com>
2024-05-05 12:51:32 +0300

    completed lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [num for num in num_list if num % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + '!' for sentence in sentence_list]
+
+if __name__ == "__main__":
+    num_list = range(10)
+    print(return_evens(num_list))
+
+    sentence_list = [
+        "I like computers",
+        "I require coffee",
+        "Live long and prosper",
+    ]
+    print(make_exclamation(sentence_list))
